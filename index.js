f48b39cfe7e4dabea8861a1b641402b091c9ceb9
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-12-14 09:42:16 -0500

    Add fn to add elves to screen

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const elf = document.getElementById('elf')
+const elfHangoutZone = document.getElementById('elf-hangout-zone')
 const btn = document.getElementById('btn')
 
 btn.addEventListener('click', duplicateElf)
 
+let numOfElves = 1
+
 function duplicateElf() {
-  // Task:
-  // - Write a function to duplicate the elf when the button is clicked.
-  // - See index.css for optional styling challenges.
+  if (numOfElves < 100) {
+    numOfElves++
+    const elf = document.createElement('div')
+    elf.className = 'elf'
+    elf.textContent = '🧝'
+    elfHangoutZone.appendChild(elf)
+  }
 }
+
+// Task:
+// - Write a function to duplicate the elf when the button is clicked.
+// - See index.css for optional styling challenges.
 
 // Stretch goals:
 // - Write a function to give the elves some tools, or a cup of tea!
